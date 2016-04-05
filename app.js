a46b5780300c5b0a5d39bed4d4ed506481d0011e
taandrighetti <taandrighetti@uc.cl>
2016-04-05 13:10:14 -0300

    minimal API

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+'use strict';
+
 const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
 app.use(morgan('tiny'))
 
+let messages = [];
+
 app.get('/', (req, res) => {
   // Send JSON
-  res.send({ status: 'on' })
+  res.send({ messages: messages })
 })
+
+// POST localhost:3000?m=hola
+app.post('/', (req, res) => {
+  messages.push(req.query.m);
+  res.send();
+});
 
 module.exports = app
